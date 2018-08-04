322ad584516e2528ec39cd275f6a5c71ad1663d9
afsal <afsals@qburst.com>
2018-08-04 21:08:09 +0530

    Added method for listing starred, shared with and trashed folder and files

--- drive3client.py
+++ drive3client.py
@@ -60,3 +60,122 @@ class Client():
                 break
         return folder_details
 
+
+    def get_starred_folder(self):
+        """
+        Fetch all the starred folder from google drive
+        :return list of folders
+        """
+        folder_details = []
+        page_token = None
+        while True:
+            response = self.service.files().list(q="starred = True and mimeType = 'application/vnd.google-apps.folder'", 
+                                                    spaces='drive',
+                                                    fields='nextPageToken, files(id, name)',
+                                                    pageToken=page_token).execute()
+            for file in response.get('files', []):
+                folder_details.append({"file_name": file.get("name"), "id": file.get("id")})
+            page_token = response.get('nextPageToken', None)
+            if page_token is None:
+                break
+        return folder_details
+    
+
+    def get_starred_files(self):
+        """
+        Fetch all the starred files from drive
+        :return list of files
+        """
+        file_details = []
+        page_token = None
+        while True:
+            response = self.service.files().list(q="starred = True and mimeType != 'application/vnd.google-apps.folder'", 
+                                                    spaces='drive',
+                                                    fields='nextPageToken, files(id, name)',
+                                                    pageToken=page_token).execute()
+            for file in response.get('files', []):
+                file_details.append({"file_name": file.get("name"), "id": file.get("id")})
+            page_token = response.get('nextPageToken', None)
+            if page_token is None:
+                break
+        return file_details
+    
+
+    def get_trashed_folder(self):
+        """
+        Fetch all trashed folder
+        :return list of folders
+        """
+        folder_details = []
+        page_token = None
+        while True:
+            response = self.service.files().list(q="trashed = True and mimeType = 'application/vnd.google-apps.folder'", 
+                                                    spaces='drive',
+                                                    fields='nextPageToken, files(id, name)',
+                                                    pageToken=page_token).execute()
+            for file in response.get('files', []):
+                folder_details.append({"file_name": file.get("name"), "id": file.get("id")})
+            page_token = response.get('nextPageToken', None)
+            if page_token is None:
+                break
+        return folder_details
+    
+
+    def get_trashed_files(self):
+        """
+        Fetch all the trashed files from drive
+        :return list of files
+        """
+        file_details = []
+        page_token = None
+        while True:
+            response = self.service.files().list(q="trashed = True and mimeType != 'application/vnd.google-apps.folder'", 
+                                                    spaces='drive',
+                                                    fields='nextPageToken, files(id, name)',
+                                                    pageToken=page_token).execute()
+            for file in response.get('files', []):
+                file_details.append({"file_name": file.get("name"), "id": file.get("id")})
+            page_token = response.get('nextPageToken', None)
+            if page_token is None:
+                break
+        return file_details
+
+    
+    def get_shared_folder(self):
+        """
+        Fetch all folder shared with me
+        :return list of folders
+        """
+        folder_details = []
+        page_token = None
+        while True:
+            response = self.service.files().list(q="sharedWithMe = True and mimeType = 'application/vnd.google-apps.folder'", 
+                                                    spaces='drive',
+                                                    fields='nextPageToken, files(id, name)',
+                                                    pageToken=page_token).execute()
+            for file in response.get('files', []):
+                folder_details.append({"file_name": file.get("name"), "id": file.get("id")})
+            page_token = response.get('nextPageToken', None)
+            if page_token is None:
+                break
+        return folder_details
+    
+
+    def get_shared_file(self):
+        """
+        Fetch all the files shared with me
+        :return list of files
+        """
+        file_details = []
+        page_token = None
+        while True:
+            response = self.service.files().list(q="sharedWithMe = True and mimeType != 'application/vnd.google-apps.folder'", 
+                                                    spaces='drive',
+                                                    fields='nextPageToken, files(id, name)',
+                                                    pageToken=page_token).execute()
+            for file in response.get('files', []):
+                file_details.append({"file_name": file.get("name"), "id": file.get("id")})
+            page_token = response.get('nextPageToken', None)
+            if page_token is None:
+                break
+        return file_details
